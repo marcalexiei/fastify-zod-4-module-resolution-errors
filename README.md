c44a47c4dbc4289c2655ac8b3956a0737204ca6f
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-06-09 13:19:28 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# fastify-zod-4-module-resolution-errors
+
+## Setup
+
+Use node 22
+
+```sh
+npm i
+```
+
+## Server errors
+
+```sh
+npm run start:js
+```
+
+```sh
+npm run start:ts
+```
+
+## Typecheck errors
+
+```sh
+npm run typecheck
+```
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod/v4";
+
+const app = Fastify();
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  // Define your schema
+  schema: {
+    querystring: z.object({
+      name: z.string().min(4),
+    }),
+    response: {
+      200: z.object({ status: z.string() }),
+    },
+  },
+  handler: (req, res) => {
+    res.send(req.query.name);
+  },
+});
+
+app.listen({ port: 4949 });
